ec3b25d4a4bfd2a9f9f1bc37b787d05e4e0aeb50
Thien Bui <thienvamai@gmail.com>
2016-09-27 00:21:24 +0700

    ckeditor

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module Multichoice
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
